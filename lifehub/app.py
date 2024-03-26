0c74c1dd67b6f45cf85cb5feec3413955cca5117
Bernardo Jordão <bernardo.jordao@tecnico.ulisboa.pt>
2024-03-26 23:15:09 +0000

    add trading212 and total to demo app

--- lifehub/app.py
+++ lifehub/app.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 from lifehub.api.ynab import YNAB
+from lifehub.api.trading212 import Trading212
 
 
 def main():
@@ -9,8 +10,22 @@ def main():
 
     accounts = ynab.get_accounts()
 
+    total_balance = 0
+
     for a in accounts:
         print(f"{a.name}: {a.balance}")
+        total_balance += a.balance
+
+    t212 = Trading212()
+
+    account = t212.get_account()
+
+    print(f"Trading212: {account['total']}")
+    total_balance += account["total"]
+
+    print("")
+
+    print(f"Total: {round(total_balance, 2)}")
 
 
 if __name__ == "__main__":
