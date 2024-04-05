449654e573ca2964eb0f6a1bbc1f36880bb0d028
Bernardo Jordão <bernardo.jordao@tecnico.ulisboa.pt>
2024-04-05 20:37:35 +0100

    add more data do networth

--- lifehub/app/fetch_scripts/fetch_networth.py
+++ lifehub/app/fetch_scripts/fetch_networth.py
@@ -13,14 +13,25 @@ def fetch_networth(
 
     accounts = ynab.get_accounts()
 
-    cash = sum([a.balance for a in accounts])
+    bank_cash = sum([a.balance for a in accounts])
 
     if t212 is None:
         t212 = Trading212()
 
-    investments = t212.get_account_cash().total
+    t212_cash = t212.get_account_cash()
 
-    networth = Networth(cash=round(cash, 2), investments=round(investments, 2))
+    uninvested_cash = round(t212_cash.free, 2)
+    invested = round(t212_cash.invested, 2)
+    returns = round(t212_cash.result, 2)
+    total = bank_cash + uninvested_cash + invested + returns
+
+    networth = Networth(
+        bank_cash=bank_cash,
+        uninvested_cash=uninvested_cash,
+        invested=invested,
+        returns=returns,
+        total=total,
+    )
 
     db_session.add(networth)
     db_session.commit()
--- lifehub/models/finance/networth.py
+++ lifehub/models/finance/networth.py
@@ -8,5 +8,8 @@ class Networth(SQLModel, table=True):
     timestamp: datetime.datetime = Field(
         default_factory=datetime.datetime.now, primary_key=True
     )
-    cash: Decimal = Field(default=0, decimal_places=2, max_digits=10)
-    investments: Decimal = Field(default=0, decimal_places=2, max_digits=10)
+    bank_cash: Decimal = Field(default=0, decimal_places=2, max_digits=10)
+    uninvested_cash: Decimal = Field(default=0, decimal_places=2, max_digits=10)
+    invested: Decimal = Field(default=0, decimal_places=2, max_digits=10)
+    returns: Decimal = Field(default=0, decimal_places=2, max_digits=10)
+    total: Decimal = Field(default=0, decimal_places=2, max_digits=10)
