d0457ebb1531dc3f0eeb0360f1c97cfda8b64a8f
Bernardo Jordão <bernardo.jordao@tecnico.ulisboa.pt>
2024-04-05 09:34:26 +0100

    create db tables from models

--- lifehub/app/db.py
+++ lifehub/app/db.py
@@ -1,6 +1,11 @@
 import os
 
+from dotenv import load_dotenv
 from sqlmodel import Session, SQLModel, create_engine
+
+import lifehub.models  # noqa: F401
+
+load_dotenv()
 
 # Database Connection String
 DATABASE_URL = os.getenv("DATABASE_URL")  # Get from environment or hardcode below
@@ -11,8 +16,8 @@ engine = create_engine(DATABASE_URL)  # echo=True for debugging SQL statements
 try:
     # Function to initialize database tables
     SQLModel.metadata.create_all(engine)
-except:
-    print("FAILED")
+except Exception as e:
+    print("Error creating database tables: ", e)
 
 
 # Function to get a database session
---
+++ lifehub/models/__init__.py
@@ -0,0 +1,3 @@
+from .finance import Networth, T212Transaction
+
+__all__ = ["Networth", "T212Transaction"]
