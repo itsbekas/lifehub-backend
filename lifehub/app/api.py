75c26684d4e4eb9b602741cfd80fe69a53a682d9
Bernardo Jordão <bernardo.jordao@tecnico.ulisboa.pt>
2024-04-13 09:42:40 +0100

    put api under /api/v0

--- lifehub/app/api.py
+++ lifehub/app/api.py
@@ -2,7 +2,7 @@ from os import getenv
 
 import uvicorn
 from dotenv import load_dotenv
-from fastapi import FastAPI
+from fastapi import APIRouter, FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 load_dotenv()
@@ -15,13 +15,13 @@ from lifehub.api.routers import (  # noqa: E402
 )
 
 #### Config ####
-api = FastAPI(
+app = FastAPI(
     title="LifeHub API",
     description="API for LifeHub",
     version="0.1.0",
-    openapi_url="/openapi.json",
-    docs_url="/docs",
-    redoc_url="/redoc",
+    openapi_url="/api/v0/openapi.json",
+    docs_url="/api/v0/docs",
+    redoc_url="/api/v0/redoc",
 )
 
 #### CORS ####
@@ -29,7 +29,7 @@ origins = [
     "http://localhost:5173",
 ]
 
-api.add_middleware(
+app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
@@ -38,15 +38,19 @@ api.add_middleware(
 )
 
 #### Routers ####
+api = APIRouter()
 api.include_router(auth_router)
 api.include_router(finance_router)
 api.include_router(tasks_router)
 api.include_router(server_router)
 
+# TODO: Eventually replace this with a reverse proxy
+app.include_router(api, prefix="/api/v0")
+
 
 def run():
     host = getenv("UVICORN_HOST", "localhost")
-    uvicorn.run("lifehub.app.api:api", host=host, port=8000, reload=True)
+    uvicorn.run("lifehub.app.api:app", host=host, port=8000, reload=True)
 
 
 if __name__ == "__main__":
