0847048a1d30e0914131d33bf717619fc542fa91
Bernardo Jordão <bernardo.jordao@tecnico.ulisboa.pt>
2024-05-11 00:23:16 +0100

    fix: improve type safety in module repository

--- lifehub/core/module/repository/module.py
+++ lifehub/core/module/repository/module.py
@@ -11,14 +11,14 @@ class ModuleRepository(BaseRepository[Module]):
 
     def get_by_id(self, id: int, retrieve_users: bool = False) -> Module | None:
         query = select(Module).filter(Module.id == id)
-        module = self.session.exec(query).first()
-        if retrieve_users:
+        module = self.session.execute(query).scalar_one_or_none()
+        if module is not None and retrieve_users:
             module.users
         return module
 
     def get_by_name(self, name: str, retrieve_users: bool = False) -> Module | None:
         query = select(Module).filter(Module.name == name)
-        module = self.session.exec(query).first()
-        if retrieve_users:
+        module = self.session.execute(query).scalar_one_or_none()
+        if module is not None and retrieve_users:
             module.users
         return module
